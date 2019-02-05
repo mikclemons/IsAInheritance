94a94a8105affd015e9aa2cef2984810231f9f62
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-02-05 14:22:03 -0500

    Extend DeveloperApprentice from the Apprentice class. Create parameterized constructor for the DeveloperApprentice class. Override the printLabberDetails() to print the unique property of the DeveloperApprentice class.

--- src/com/company/DeveloperApprentice.java
+++ src/com/company/DeveloperApprentice.java
@@ -1,4 +1,20 @@
 package com.company;
 
-public class DeveloperApprentice {
+import java.util.ArrayList;
+
+public class DeveloperApprentice extends Apprentice {
+
+    private ArrayList<String> programmingLanguages = new ArrayList<String>();
+
+    public DeveloperApprentice(ArrayList<String> programmingLanguages, String greekClassLetter, boolean isGraduated, String firstName, String lastName, String jobType, String lengthOfEmployment){
+        super(greekClassLetter, isGraduated, firstName, lastName, jobType, lengthOfEmployment);
+
+    }
+
+    @Override
+    public void printLabberDetails(){
+        System.out.println("This developer apprentice knows the following programming languages: " + programmingLanguages);
+
+
+    }
 }
