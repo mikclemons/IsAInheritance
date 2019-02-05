3e48ead25594080fab05e68c90f47e8f35b256f8
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-02-05 13:53:06 -0500

    Override the printLabberDeatils() in the Apprentice class to print the Apprentice details.

--- src/com/company/Apprentice.java
+++ src/com/company/Apprentice.java
@@ -3,13 +3,17 @@ package com.company;
 public class Apprentice extends DetroitLabber {
 
     private String greekClassLetter;
-    private String isGraduated;
+    private boolean isGraduated;
 
-    public Apprentice(String greekClassLetter, String isGraduated, String firstName, String lastName, String jobType, String lengthOfEmployment){
+    public Apprentice(String greekClassLetter, boolean isGraduated, String firstName, String lastName, String jobType, String lengthOfEmployment){
         super(firstName, lastName, jobType, lengthOfEmployment);
         this.greekClassLetter  = greekClassLetter;
         this.isGraduated = isGraduated;
 
+    }
+    @Override
+    public void printLabberDetails(){
+        System.out.println("This apprentice is in the: " + greekClassLetter + "class and it is " + isGraduated + "that they have graduated.");
     }
 
 }
